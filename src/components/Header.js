defed129b2a245e2cb1010721e3f74136ccf83c6
chinweibegbu <chinwe.ibegbu@gmail.com>
2023-09-06 22:16:06 +0100

    Created a simple test-driven component

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from '../logo.svg';
+
+function Header() {
+  return (
+    <div className="Header">
+      <h3>This is a test-driven header component</h3>
+    </div>
+  );
+}
+
+export default Header;
