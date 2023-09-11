948166fd33bfe707a2e3bb76b19a6bddb602dcb3
chinweibegbu <chinwe.ibegbu@gmail.com>
2023-09-11 12:54:17 +0100

    Mocked API call using Axios

--- src/containers/StoreLocator.js
+++ src/containers/StoreLocator.js
@@ -1,26 +1,26 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../components/Header';
 import Button from '../components/Button';
 import Map from '../components/Map';
 import mapChooser from '../MapChooser';
+import axios from 'axios';
 
 function StoreLocator() {
-    const [currentMap, setCurrentMap] = useState('none.png')
+    const [currentMap, setCurrentMap] = useState('none.png');
+    const [stores, setStores] = useState([]);
 
     const chooseStore = (e) => {
         setCurrentMap(mapChooser(e.target.value));
     }
 
-    const stores = [{
-        'location': 'Portland',
-        'address': '123 Portland Dr'
-    }, {
-        'location': 'Astoria',
-        'address': '123 Astoria Dr'
-    }, {
-        'location': '',
-        'address': ''
-    }];
+    const fetchData = async () => {
+        const response = await axios.get('http://localhost:3000/data/stores.json');
+        setStores(response.data.stores);
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, []);
 
     const storeButtons = stores.map((store, id) => {
         return <Button handleClick={chooseStore} key={id} location={store.location} />
